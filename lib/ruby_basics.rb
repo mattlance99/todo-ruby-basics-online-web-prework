411e019f2b36f80cf58ac21a279e09f06448b97f
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-12 16:03:12 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,21 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  person = value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
-def return_a_value
-
+def return_a_value(phrase = "Nice")
+  phrase
 end
 
 def last_evaluated_value
